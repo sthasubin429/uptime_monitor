910b50be379fa83b805aa863569ee506e8a09dd0
subin <48222247+sthasubin429@users.noreply.github.com>
2024-06-07 16:06:03 +0545

    fix: basic logic for uptime monitoring

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use reqwest;
+use std::error::Error;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let url = "http://google.com";
+
+    let response = reqwest::get(url).await;
+
+    match response {
+        Ok(resp) => {
+            if resp.status().is_success() {
+                println!("The website is up!");
+                println!("Status code: {}", resp.status());
+            } else {
+                println!("The website is down! Status code: {}", resp.status());
+            }
+        }
+        Err(err) => {
+            println!("Failed to check the website. Error: {}", err);
+        }
+    }
+
+    Ok(())
 }
